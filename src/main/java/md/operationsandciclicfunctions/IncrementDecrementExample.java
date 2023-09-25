3d460435a2c9b672546de0a1e803f5fec85e1826
Lenovo <andreykapm@gmail.com>
2023-09-25 20:53:55 +0300

    Added the implimentation of java opertation, cyclic Functions and control structures.

---
+++ src/main/java/md/operationsandciclicfunctions/IncrementDecrementExample.java
@@ -0,0 +1,28 @@
+package md.operationsandciclicfunctions;
+
+public class IncrementDecrementExample {
+    public static void main(String[] args) {
+        int a = 89;
+        int b = 34;
+
+        System.out.println(a++);
+        System.out.println(a);
+
+        System.out.println(++b);
+
+
+        a = a + 1;
+        System.out.println(a);
+
+        System.out.println(a++);
+
+        System.out.println(a);
+
+        System.out.println(--b);
+
+        b = --a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+    }
